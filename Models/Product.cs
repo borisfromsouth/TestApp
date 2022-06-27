8410c380111b79df04e941feb0e755b0ac570b3f
boris_from_south <sashaborisenko@tut.by>
2022-06-27 17:13:06 +0300

    add: minor features
    add: minor features

--- Models/Product.cs
+++ Models/Product.cs
@@ -3,13 +3,16 @@
     public class Product
     {
         public string Name { get; set; }
+        public string Category { get; set; } = "Watersports";
         public decimal? Price { get; set; }
         public Product Related { get; set; }
         public static Product[] GetProducts()
         {
             Product kayak = new Product
             {
-                Name = "Kayak", Price = 275M
+                Name = "Kayak", 
+                Category = "Water Craft",
+                Price = 275M
             };
             Product lifeJacket = new Product
             {
