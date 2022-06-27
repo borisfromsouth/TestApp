8a1ad5ad91ab8ba41b53d476d1bc5ee29477589b
boris_from_south <sashaborisenko@tut.by>
2022-06-27 17:06:38 +0300

    Initial commit

---
+++ Controllers/Home/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using TestApp.Models;
+
+namespace TestApp.Controllers.Home
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            List<string> results = new List<string>();
+            foreach (Product p in Product.GetProducts())
+            {
+                string name = p?.Name;
+                decimal? price = p?.Price;
+                string relatedName = p?.Related?.Name;
+                results.Add(string.Format("Name: {0}, Price: {1}, Related: {2}", name, price, relatedName));
+            }
+            return View(results);
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,25 @@
+﻿namespace TestApp.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal? Price { get; set; }
+        public Product Related { get; set; }
+        public static Product[] GetProducts()
+        {
+            Product kayak = new Product
+            {
+                Name = "Kayak", Price = 275M
+            };
+            Product lifeJacket = new Product
+            {
+                Name = "lifeJacket",
+                Price = 48.95M
+            };
+
+            kayak.Related = lifeJacket; ;
+
+            return new Product[] { kayak, lifeJacket, null };
+        }
+    }
+}
